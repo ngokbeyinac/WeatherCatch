b5773d880a436392e793540078a980a6b5d8b3d6
ngokbeyinac <112663336+ngokbeyinac@users.noreply.github.com>
2023-06-02 21:54:52 +0300

    Initial Commit

---
+++ WeatherCatch/WeatherCatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCatchApp.swift
+//  WeatherCatch
+//
+//  Created by Nureddin Gökbey İnaç on 2.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
